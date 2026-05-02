740ebe3f04dff843c665550056d665f53255585e
이정현 <wjd1gus2@gmail.com>
2026-05-02 22:08:00 +0900

    fix: clean up process_exec argument cleanup

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -223,11 +223,10 @@ setup_argument_stack (char **argv, int argc, struct intr_frame *if_) {
  * 실패하면 -1을 반환한다. */
 int
 process_exec (void *f_name) {
-	if (f_name == NULL)
-		return -1;
+	ASSERT (f_name != NULL);
 
 	char *file_name = f_name;
-	bool success;
+	bool success = false;
 
 	/* thread 구조체 안의 intr_frame은 사용할 수 없다.
 	 * 현재 스레드가 다시 스케줄될 때 실행 정보를 그 멤버에 저장하기 때문이다. */
@@ -242,23 +241,17 @@ process_exec (void *f_name) {
 
 	char *argv[MAX_ARGC];
 	int argc = parse_command_line (file_name, argv);
-	if (argc == -1) {
-		palloc_free_page (file_name);
-		return -1;
+	if (argc != -1) {
+		/* 그런 다음 바이너리를 로드한다. */
+		success = load (argv[0], &_if);
+		if (success)
+			setup_argument_stack (argv, argc, &_if);
 	}
-
-	/* 그런 다음 바이너리를 로드한다. */
-	success = load (argv[0], &_if);
-
-	/* load에 실패했으면 종료한다. */
-	if (!success) {
-		palloc_free_page (file_name);
-		return -1;
-	}
-
-	setup_argument_stack (argv, argc, &_if);
 
 	palloc_free_page (file_name);
+	if (!success)
+		return -1;
+
 	/* 전환된 프로세스를 시작한다. */
 	do_iret (&_if);
 	NOT_REACHED ();
